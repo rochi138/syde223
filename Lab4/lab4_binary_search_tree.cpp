e959d6c9940fc0ac8dd0376329b95faed739a1a6
rjching138 <38929552+rjching138@users.noreply.github.com>
2019-03-20 16:12:07 -0400

    Test 1

--- Lab4/lab4_binary_search_tree.cpp
+++ Lab4/lab4_binary_search_tree.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include "lab4_binary_search_tree.hpp"
+#include <sstream>
+#include <queue>
 
 using namespace std;
 
 // PURPOSE: Default/empty constructor
 BinarySearchTree::BinarySearchTree() {
+	root = NULL;
+	size = 0;
 }
 
 // PURPOSE: Explicit destructor of the class BinarySearchTree
 BinarySearchTree::~BinarySearchTree() {
+	if (root == NULL)
+		return;
 }
 
 // PURPOSE: Returns the number of nodes in the tree
 unsigned int BinarySearchTree::get_size() const {
-	return 0;
+	return size;
 }
 
 // PURPOSE: Returns the maximum value of a node in the tree
@@ -35,6 +41,29 @@ unsigned int BinarySearchTree::height() const {
 
 // PURPOSE: Prints the contents of the tree; format not specified
 void BinarySearchTree::print() const {
+	cout<<endl<<endl<<endl<<endl<<endl<<"see"<<endl;
+	if (!root) {
+    	cout<<""<<endl;
+    }    
+
+	stringstream ss;
+	queue<BinarySearchTree::TaskItem*> node_queue;
+	node_queue.push(root);
+	while (!node_queue.empty()) {
+		BinarySearchTree::TaskItem* cur_node = node_queue.front();
+		node_queue.pop();
+		ss << cur_node->priority << " ";
+		if (cur_node->left) {
+			node_queue.push(cur_node->left);
+		}
+		if (cur_node->right) {
+			node_queue.push(cur_node->right);
+		}
+	}
+    
+	string level_order_str = ss.str();
+
+	cout<<level_order_str.substr(0, level_order_str.size() - 1)<<endl;
 }
 
 // PURPOSE: Returns true if a node with the value val exists in the tree	
@@ -61,7 +90,34 @@ int BinarySearchTree::get_node_depth( BinarySearchTree::TaskItem* n ) const {
 // PURPOSE: Inserts the value val into the tree if it is unique
 // returns true if successful; returns false if val already exists
 bool BinarySearchTree::insert( BinarySearchTree::TaskItem val ) {
-    return false;
+	//cout<<val.priority<<endl;
+	if (root == NULL){
+		root = new TaskItem(val);
+		return true;
+	}
+	bool placed = false;
+	TaskItem* parent = root;
+	TaskItem* grandparent = NULL;
+	while(!placed){
+		if (parent == NULL){
+			parent = new TaskItem(val);
+			if (parent->priority > grandparent->priority)
+				grandparent->right = parent;
+			else
+				grandparent->left = parent;
+			placed = true;
+		}
+		if (val.priority == parent->priority)
+			return false;
+		grandparent = parent;
+		if (val.priority < parent->priority){
+			parent = parent->left;
+		} else {
+			parent = parent->right;
+		}
+	}
+	//cout<<val.priority<<endl;
+    return true;
 }
 
 // PURPOSE: Removes the node with the value val from the tree
